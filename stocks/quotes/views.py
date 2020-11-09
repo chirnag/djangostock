ee3cb28295bd738a3596247db00f65b64d9dc8a1
Nag <nagarjuna.chirala@gmail.com>
2020-11-08 22:25:33 -0700

    test commit

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    import requests 
+    import json
     # pk_adc0c18f6fdf474a8c1ba9784af51d79
+    api_request = requests.get("test api")
     return render(request, 'home.html', {})
 
 def about(request):
