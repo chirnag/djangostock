63319827d7dd2325429269a49d035698c1adb80a
Nag <nagarjuna.chirala@gmail.com>
2020-11-09 23:00:37 -0700

    checkin for today

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -6,8 +6,14 @@ def home(request):
     import requests 
     import json
     # pk_adc0c18f6fdf474a8c1ba9784af51d79
-    api_request = requests.get("test api")
-    return render(request, 'home.html', {})
+    
+    try:
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote/latestPrice?token=pk_adc0c18f6fdf474a8c1ba9784af51d79")
+        api = json.loads(api_request.content)
+    except Exception as identifier:
+        api = identifier        
+
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
