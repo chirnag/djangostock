0497bdd8db85fe3609431adb78e1350ed0b622e3
Nag <nagarjuna.chirala@gmail.com>
2020-11-10 21:27:28 -0700

    today's commit

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from .models import Stock
+from .forms import StockForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -6,14 +10,35 @@ def home(request):
     import requests 
     import json
     # pk_adc0c18f6fdf474a8c1ba9784af51d79
-    
-    try:
-        api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote/latestPrice?token=pk_adc0c18f6fdf474a8c1ba9784af51d79")
-        api = json.loads(api_request.content)
-    except Exception as identifier:
-        api = identifier        
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        try:
+            api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote/?token=pk_adc0c18f6fdf474a8c1ba9784af51d79")
+            api = json.loads(api_request.content)
+        except Exception as identifier:
+            api = "Error"        
+        return render(request, 'home.html', {'api': api})
+    else:
+        return render(request, 'home.html', {'ticker': "Enter a Ticker symbol above"})    
 
-    return render(request, 'home.html', {'api': api})
+    
 
 def about(request):
     return render(request, 'about.html', {})
+
+def add_stock(request):
+    if request.method == 'POST':
+        form = StockForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Stock has been added"))
+            return redirect('add_stock')
+    else:
+        ticker = Stock.objects.all()
+        return render(request, 'add_stock.html', {'ticker': ticker})
+
+def delete(request, stock_id):
+    item = Stock.objects.get(pk=stock_id)
+    item.delete()
+    messages.success(request, ("Stock has been deleted"))
+    return redirect(add_stock)
